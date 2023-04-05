55f4b54cacca37244d447fdef27a58b000cd5cd4
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-04-05 11:08:17 +0300

    Using flask to add python into the html file.

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    current_year = datetime.now().year
+    my_name = "KIBIRIGE KALULE SAMUEL"
+    return render_template("index.html", num=random_number, year=current_year, name=my_name)
 
 
 if __name__ == "__main__":
